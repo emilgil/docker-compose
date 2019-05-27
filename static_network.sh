4dc0df28f0b59810f292d893a02a5e8ceea3c7fa
Emil Gil <emil@gildebrand.com>
2019-05-27 13:03:12 +0200

    Modified script

--- static_network.sh
+++ static_network.sh
@@ -1,2 +1,2 @@
-!#/bin/bash
-docker network create --driver=bridge --subnet=172.16.0.0/22 --ip-range=172.16.1.0/24 --gateway=172.16.3.254 br0
+#!/bin/bash
+/usr/bin/docker network create --driver=bridge --subnet=172.16.0.0/22 --ip-range=172.16.1.0/24 --gateway=172.16.3.254 br1
